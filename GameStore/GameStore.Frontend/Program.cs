0f49f9206b0fe2f3e049449623a04b2276587d0f
pockypoem <jeremyas.cornelis@gmail.com>
2024-05-04 14:29:47 +0700

    init: creating a Blazor project
    when developing Blazor, don't forget to use `dotnet watch`

---
+++ GameStore/GameStore.Frontend/Program.cs
@@ -0,0 +1,25 @@
+using GameStore.Frontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection(); // so the application isn't going to try to redirect us into https
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
