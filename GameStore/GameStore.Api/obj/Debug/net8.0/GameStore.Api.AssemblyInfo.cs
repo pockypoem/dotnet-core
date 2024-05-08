7568fa49b36ed6aab94532018fd17b94f0067365
pockypoem <jeremyas.cornelis@gmail.com>
2024-05-08 18:21:23 +0700

    chore: finished integration 🥳

--- GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a774076d887220a6ae35e8599f1f8f5d37d982a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493785c7ceb089b0cd1d319b1aed8904d9366947")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
