c4800e6a5f1edd4b555f74c0c6efcbfdcce79a45
pockypoem <jeremyas.cornelis@gmail.com>
2024-05-08 22:24:30 +0700

    delete: delete unnecessary file

--- GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5ec6da8846fd093a388226e565aca8477f3dd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50047d4098715b1c3c9058eedfea65ae9a6fc5bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
